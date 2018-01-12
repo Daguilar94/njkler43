665fabac34a60cafcfc23252b1867b80aeb7c0b7
David Aguilar <davidaguilarh.94@gmail.com>
2018-01-11 21:53:34 -0500

    fix filter functionality

--- src/App.js
+++ src/App.js
@@ -5,24 +5,47 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor(){
+    super()
+    this.updateInput = this.updateInput.bind(this)
+    this.render_post = this.render_post.bind(this)
+    this.state = {
+      keyWord: ''
+    }
+  }
+
+  render_post(post, index) {
+    const post_title = post.title.toLowerCase()
+    const keyWord = this.state.keyWord.toLowerCase()
+    if (post_title.match(keyWord)) {
+      return(
+        <li key={index}>
+          <a href={post.url}><img src={post.image } /></a>
+          <p>{post.title}</p>
+        </li>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" onChange={this.updateInput} value={this.state.keyWord}/>
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+        {posts.map(this.render_post)}
         </ul>
       </div>
     )
+  }
+
+  updateInput(e) {
+    this.setState({
+      keyWord: e.target.value
+    })
   }
 }
 
 
 export default App
-
-
